1d8c23d38c010eb8d8832190998bfaf14f82bdec
luweiymy <luweiymy@users.noreply.github.com>
2015-05-15 15:21:20 +0800

    将轮播图转成动画的，但是只实现了一小部分，还需要继续完善

--- animate/js/slide.js
+++ animate/js/slide.js
@@ -3,17 +3,16 @@
 	利用的是js加载页面
 */
 $(document).ready(function(){		
-	var count = 4,n = 1;//count为图片的总数
-	var slide = function(){//实现图片的自动播放
-
-	};
-	$(".slidepre").click(function(){//实现图片点击向左，翻页
-	
-		
-	}); 
-	$(".slidenext").click(function(){//实现图片点击向右，翻页
-			
+	var count = 4,n;//count为图片的总数
+	$(".slideimg li").click(function(){
+		$(this).find("img").animate({left:'10px'}).css("display","block");
+		$(".pageul li.pageactive").removeClass("pageactive").next().addClass("pageactive")
 	});
-	
+	var slide = function(){//实现图片的自动播放
+		n = $(".pageul li.pageactive").text() - 1;
+		$(".slideimg li").eq(n).trigger("click");
+		n--;
+		
+	};
 	var timer = setInterval(slide,5000);//设置定时器
 });
